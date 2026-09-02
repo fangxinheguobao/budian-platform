534fee4d0b88278fe58d315c7d6f302933f1cbc3
budian-dev <budian-dev@users.noreply.github.com>
2026-09-02 21:56:19 +0800

    fix: kit.jsx 缺失 useState 导入导致商城/面料库/打样页白屏

--- src/components/kit.jsx
+++ src/components/kit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { X } from 'lucide-react'
 import { fabricStatus, useDB } from '../store/db'
